1ba3087f06147ae613a2ce398f42846c08ff6374
MegaEmme <mikmusco@gmail.com>
2025-03-12 16:00:09 +0100

    solved es.8

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,7 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const studentClass = students.find((student) => {if(student.name === 'Marco Lanci'){ return student.class}});
+
+  console.log(studentClass.class);
