c842ddcf5686395c91c703f9f39a4a43b7a4a298
MegaEmme <mikmusco@gmail.com>
2025-03-12 15:36:42 +0100

    fixed es.3

--- snack-3/script.js
+++ snack-3/script.js
@@ -3,6 +3,6 @@ const numbers = [2, 8, 4, 7, 2, 87];
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
 
-const addOne = numbers.map((numbers) =>{ return numbers + 1});
+const addOne = numbers.map((number) =>{ return number + 1});
 
 console.log(addOne);
